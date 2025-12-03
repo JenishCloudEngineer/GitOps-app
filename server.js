84c49823c970650ddf18d0cc2d41c566d388b645
JenishCloudEngineer <jenishjk98@gmail.com>
2025-12-03 20:38:42 +0530

    testing gitops

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Jenish! 🚀 10000Your GitOps Node.js App is running on Kubernetes via ArgoCD.");
+  res.send("Hello Jenish! 🚀 52456Your GitOps Node.js App is running on Kubernetes via ArgoCD.");
 });
 
 const PORT = process.env.PORT || 3000;
