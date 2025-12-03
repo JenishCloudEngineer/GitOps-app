6b6ea0d554dd487a37dbee6e9152aad263f7a689
JenishCloudEngineer <jenishjk98@gmail.com>
2025-12-03 13:37:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Jenish! 🚀 Your GitOps Node.js App is running on Kubernetes via ArgoCD.");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Node.js app running on port ${PORT}`);
+});
