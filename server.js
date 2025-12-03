174bb740c56ce2e2f9b86b3464cfacdc21d275ae
JenishCloudEngineer <jenishjk98@gmail.com>
2025-12-03 19:09:33 +0530

    testing the app for full pipeline

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Jenish! 🚀 Your GitOps Node.js App is running on Kubernetes via ArgoCD,updated.");
+  res.send("Hello Jenish! 🚀 Your GitOps Node.js App is running on Kubernetes via ArgoCD.");
 });
 
 const PORT = process.env.PORT || 3000;
