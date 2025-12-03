0e350e0c4bef6eb28d47f7af85f02b945151971c
JenishCloudEngineer <jenishjk98@gmail.com>
2025-12-03 16:32:39 +0530

    updated version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Jenish! 🚀 Your GitOps Node.js App is running on Kubernetes via ArgoCD,100.");
+  res.send("Hello Jenish! 🚀 Your GitOps Node.js App is running on Kubernetes via ArgoCD,updated.");
 });
 
 const PORT = process.env.PORT || 3000;
